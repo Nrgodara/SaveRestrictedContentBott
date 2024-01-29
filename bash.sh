3a2ef3b486414f1a680b474eb9dfcc9025d6bb8a
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-01-29 06:23:55 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,7 @@
-echo "starting Bot ~@DroneBots";
-python3 -m main
+echo "Starting Bot ~@DroneBots";
+while true; do
+    python3 -m main
+    # Add a sleep/delay here (e.g., sleep 1) to control the speed
+    sleep 1
+done
+
