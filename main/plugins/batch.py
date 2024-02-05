8feebd347307339b3009db44fe1e6af6b38afdac
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-02-05 18:39:46 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -112,4 +112,3 @@ async def run_batch(userbot, client, sender, link, _range):
         protection = await client.send_message(sender, f"Sleeping 😴 for `{timer}` seconds to avoid Floodwaits and Protect account!")
         await asyncio.sleep(timer)
         await protection.delete()
-            
